f3e0417cb9f9c2f95e5ee8225b48a5d743cd6f93
Vishak A Kamath <vishak@creoit.com>
2018-12-13 18:23:09 +0530

    Initial commit

--- src/com/java/package1/Main.java
+++ src/com/java/package1/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         new com.java.package1.Square().showDetails();
+        System.out.println("Hello world");
     }
 }
